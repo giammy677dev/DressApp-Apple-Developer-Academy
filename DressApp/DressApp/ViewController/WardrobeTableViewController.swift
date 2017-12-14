6701efe4284bd79ed6a8b3f08b225381a45b8859
Giulio Izzo <giulioizzo@10-20-49-191.0200.fed.it.iosda.org>
2017-12-14 09:33:00 +0100

    Tab bar fixed and cell heights modified

--- DressApp/DressApp/ViewController/WardrobeTableViewController.swift
+++ DressApp/DressApp/ViewController/WardrobeTableViewController.swift
@@ -25,18 +25,26 @@ class WardrobeTableViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        
+        return 77
+    }
+    
+    
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
-
+//    override func numberOfSections(in tableView: UITableView) -> Int {
+//        // #warning Incomplete implementation, return the number of sections
+//        return 0
+//    }
+//
+//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        // #warning Incomplete implementation, return the number of rows
+//        return 0
+//    }
+//
+//    
     /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
