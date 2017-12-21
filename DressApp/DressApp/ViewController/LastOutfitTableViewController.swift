80c0f4d0d8d8a94639e2300df13824104f794c35
Gian Marco Orlando <gianmarcoorlando@macbook-pro-di-gian.local>
2017-12-21 03:15:29 +0100

    Push delle 3

---
+++ DressApp/DressApp/ViewController/LastOutfitTableViewController.swift
@@ -0,0 +1,39 @@
+//
+//  LastOutfitTableViewController.swift
+//  DressApp
+//
+//  Created by Gian Marco Orlando on 21/12/17.
+//  Copyright © 2017 Checkmate Team. All rights reserved.
+//
+
+import UIKit
+
+class LastOutfitTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return Wardrobe.shared.getLastOutfits().count
+    }
+}
